e34bb929be470a98832a3fdb5f60c771d0b7ff4b
Emre Sert <emresert1233@gmail.com>
2019-05-05 21:46:58 +0300

    group message show with id is completed

--- Agm/Controllers/TextMessageController.cs
+++ Agm/Controllers/TextMessageController.cs
@@ -1,4 +1,5 @@
 ﻿using Agm.Hubs;
+using Agm.Models.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -15,40 +16,47 @@ namespace Agm.Controllers
         // GET: TextMessage
         public ActionResult Index()
         {
+            var user = Session["User"] as Users;
+            if (user == null)
+            {
+                return RedirectToAction("Login", "Home");
+            }
             return View();
         }
-        public JsonResult Get()
+        public JsonResult Get(int id)
         {
-            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["MsgConnection"].ConnectionString))
-            {
-                connection.Open();
-                using (SqlCommand command = new SqlCommand(@"SELECT [textId],[textOwner],[textContent],[groupFk] from [dbo].[TextMessage]", connection))
+           if(id >0) {
+                using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["MsgConnection"].ConnectionString))
                 {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(@"SELECT [textId],[textOwner],[textContent],[groupFk] from [dbo].[TextMessage] where groupFk=" + id, connection))
+                    {
 
-                    command.Notification = null;
+                        command.Notification = null;
 
-                    SqlDependency dependency = new SqlDependency(command);
-                    dependency.OnChange += new OnChangeEventHandler(dependency_OnChange);
+                        SqlDependency dependency = new SqlDependency(command);
+                        dependency.OnChange += new OnChangeEventHandler(dependency_OnChange);
 
-                    if (connection.State == ConnectionState.Closed)
-                        connection.Open();
+                        if (connection.State == ConnectionState.Closed)
+                            connection.Open();
 
-                    SqlDataReader reader = command.ExecuteReader();
+                        SqlDataReader reader = command.ExecuteReader();
 
-                    var listMsg = reader.Cast<IDataRecord>()
-                            .Select(x => new
-                            {
-                                textId = (int)x["textId"],
-                                textOwner = (string)x["textOwner"],
-                                textContent = (string)x["textContent"],
-                                groupFk = (int)x["groupFk"]
-                             
-                            }).ToList();
+                        var listMsg = reader.Cast<IDataRecord>()
+                                .Select(x => new
+                                {
+                                    textId = (int)x["textId"],
+                                    textOwner = (string)x["textOwner"],
+                                    textContent = (string)x["textContent"],
+                                    groupFk = (int)x["groupFk"],
 
-                    return Json(new { listMsg = listMsg }, JsonRequestBehavior.AllowGet);
+                                }).ToList();
 
-                }
-            }
+                        return Json(new { listMsg = listMsg }, JsonRequestBehavior.AllowGet);
+                    }
+                }          
+            }     
+           return new JsonResult();
         }
         public void dependency_OnChange(object sender, SqlNotificationEventArgs e)
         {
