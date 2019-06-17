2aa6728dd6efc8bc1dc477324c7d54fd6dd2ac37
Emre Sert <emresert1233@gmail.com>
2019-06-18 02:08:49 +0300

    user edit is completed and thesis line is here

--- Agm/Controllers/UsersController.cs
+++ Agm/Controllers/UsersController.cs
@@ -86,11 +86,93 @@ namespace Agm.Controllers
             change.userLoginName = editUser.userLoginName;
             change.userPhoneNumber = editUser.userPhoneNumber;
             change.userPassword = editUser.userPassword;
+            change.reuserPassword = editUser.userPassword;
             change.userNameSurname = editUser.userNameSurname;
             ViewBag.uImage = editUser.userImageUrl;
             change.userImageUrl = editUser.userImageUrl;
 
             return View(change);
+        }
+        [HttpPost]
+        public ActionResult Edit(usersModel uModel, HttpPostedFileBase file)
+        {
+            var user = Session["User"] as Users;
+            if (user == null)
+            {
+                return RedirectToAction("Login", "Home");
+            }
+
+            var editUser = db.Users.FirstOrDefault(u => u.userId == user.userId);
+
+            editUser.userId = user.userId;
+            editUser.userNameSurname = uModel.userNameSurname;
+
+            if(user.userLoginName != uModel.userLoginName)
+            {
+                var testLoginName = db.Users.FirstOrDefault(t => t.userLoginName == uModel.userLoginName);
+                if(testLoginName == null)
+                {
+                    editUser.userLoginName = uModel.userLoginName;
+                }
+                else
+                {
+                    TempData["msg"] = "<script>alert('Bu kullanıcı adında zaten bir kullanıcı var.')</script>";
+                    editUser.userLoginName = user.userLoginName;
+                    return RedirectToAction("Edit");
+                }
+            }
+            if(user.userPhoneNumber != uModel.userPhoneNumber)
+            {
+                var testPhoneNumber = db.Users.FirstOrDefault(t => t.userPhoneNumber == uModel.userPhoneNumber);
+                if (testPhoneNumber == null)
+                {
+                    editUser.userPhoneNumber = uModel.userPhoneNumber;
+                }
+                else
+                {
+                    TempData["msg"] = "<script>alert('Bu telefon numarasına kayıtlı zaten bir kullanıcı var.')</script>";
+                    editUser.userPhoneNumber = user.userPhoneNumber;
+                    return RedirectToAction("Edit");
+                }
+            }
+            if (user.userEmail != uModel.userEmail)
+            {
+                var testMail = db.Users.FirstOrDefault(t => t.userEmail == uModel.userEmail);
+                if (testMail == null)
+                {
+                    editUser.userEmail = uModel.userEmail;
+                }
+                else
+                {
+                    TempData["msg"] = "<script>alert('Bu mail adresine kayıtlı zaten bir kullanıcı var.')</script>";
+                    editUser.userEmail = user.userEmail;
+                    return RedirectToAction("Edit");
+                }
+            }
+
+
+            if (file != null && file.ContentLength > 0)
+            {
+                string fileName = Path.GetFileName(file.FileName);
+                string imgPath = Path.Combine(Server.MapPath("~/User_Images/"), fileName);
+                file.SaveAs(imgPath);
+                editUser.userImageUrl = "/User_Images/" + file.FileName;
+            }
+            
+           if(uModel.userPassword != uModel.reuserPassword)
+            {
+                TempData["msg"] = "<script>alert('Girdiğiniz şifreler uyuşmuyor. Lütfen tekrar girin.')</script>";
+                return RedirectToAction("Edit");
+            }
+           else
+            {
+                editUser.userPassword = uModel.userPassword;
+            }
+
+            db.SaveChanges();
+            TempData["msg"] = "<script>alert('Değişiklikleriniz kaydedildi.')</script>";
+            return RedirectToAction("Edit");
+           
         }
         public ActionResult Logout()
         {
